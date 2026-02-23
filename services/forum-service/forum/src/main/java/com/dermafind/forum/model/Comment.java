2e760a13ae20094e7338bcd2dac6a69a6dae5d1a
Alison Pariela <parielaalison@gmail.com>
2026-02-23 16:28:42 +0400

    Added Post entity

---
+++ services/forum-service/forum/src/main/java/com/dermafind/forum/model/Comment.java
@@ -0,0 +1,5 @@
+package com.dermafind.forum.model;
+
+public class Comment {
+    
+}
---
+++ services/forum-service/forum/src/main/java/com/dermafind/forum/model/Post.java
@@ -0,0 +1,38 @@
+package com.dermafind.forum.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="posts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private ForumUser author;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+}
